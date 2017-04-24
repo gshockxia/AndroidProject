9e5003d6b2995aae4462a98244cb8875bfdce8b4
gshockxia <gshockxia@gmail.com>
2017-04-24 16:06:44 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaaix/www/listviewtest/FruitAdapter.java
@@ -0,0 +1,37 @@
+package com.xiaaix.www.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Monster on 2017/4/24.
+ */
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+
+    public FruitAdapter(Context context, int textViewresourceId, List<Fruit> objects) {
+        super(context, textViewresourceId, objects);
+        resourceId = textViewresourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        fruitImage.setImageResource(fruit.getImageId());
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitName.setText(fruit.getName());
+        return view;
+    }
+}
